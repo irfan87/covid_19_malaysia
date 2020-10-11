0a1a8ee1b0e9f85cbaf5ea31bb7e74bc1071ab38
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-10-12 00:28:20 +0800

    change date format

--- src/components/Dashboard/MainDashboardPage.js
+++ src/components/Dashboard/MainDashboardPage.js
@@ -21,24 +21,29 @@ const useFetch = (url) => {
 			const response = await axios(url);
 
 			setData(response.data);
-
 			setLoading(false);
 		};
-
 		fetchData();
 	});
 
 	return { data, loading };
 };
 
+
 export const MainDashboardPage = () => {
+	// fetch one current result of Covid-19
 	const { data, loading } = useFetch(
 		"https://api.apify.com/v2/key-value-stores/6t65lJVfs3d8s6aKc/records/LATEST?disableRedirect=true"
 	);
 
+	// fetch all current result of Covid-19, started from day 1 to this date
+	const {allData, dataLoading} = useFetch("https://api.apify.com/v2/datasets/7Fdb90FMDLZir2ROo/items?format=json&clean=1");
+
 	return (
 		<>
 			<h2>Result</h2>
+			<button onClick={() => window.location.reload()}>Refresh</button>
+			<br />
 			{loading ? (
 				<Container>
 					<Row>
@@ -59,8 +64,8 @@ export const MainDashboardPage = () => {
 			) : (
 				// <div>Fetching the current COVID-19 Result....</div>
 				<>
-					Current Result for {" "}
-					{Moment(data["lastUpdatedAtApify"]).format("DD-MM-YYYY hh:mm:ss a")}
+					Last fetched was {" "}
+					{Moment(data["lastUpdatedAtApify"]).format("DD-MM-YYYY")} {" "} at {" "} {Moment(data['lastUpdatedAtApify']).startOf('minute').fromNow()}
 					<div className='card-container'>
 						<CovidCardBody 
 							testedPositive={data['testedPositive']} 
